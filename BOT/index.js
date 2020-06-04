22aebbadb7c21c5fbd6629556f1dae2291b60b61
MrMysterius <official.mrmysterius@gmail.com>
2020-06-04 21:07:29 +0200

    Fix some calc in cmd also added -dist & -stargate

--- BOT/index.js
+++ BOT/index.js
@@ -665,30 +665,45 @@ Bot.on("message", (msg) => {
             let rgxSpeed = new RegExp('(?<=-speed )[0-9\\.]*');
             let rgxTime = new RegExp('(?<=-time )[0-9\\:]*');
             let rgxETA = new RegExp('(?<=-eta )[0-9\\:]*');
+            let rgxDistance = new RegExp('(?<=-distance )[0-9\\.]*');
+            let rgxStargate = new RegExp('(?<=-stargate )[0-9\\.]*');
 
             //REGEX INFO VARIABLE DECLERATION
-            var sHex = -1, eHex = -1, speed = -1, time = -1, eta = -1;
+            var sHex = -1, eHex = -1, speed = -1, time = -1, eta = -1, distance = -1, stargate = -1;
 
             //GETTING ALL THE INFOS FROM THE MESSAGE
             argsFull = args.join(' ');
-            var match = argsFull.match(rgxSHex);
+            //EITHER DISTANCE OR HEXES
+            var match = argsFull.match(rgxDistance);
             if (mc(match)) {
-                sHex = match[0].split(',');
-                match = argsFull.match(rgxEHex);
+                distance = parseInt(match[0], 10);
+            } else {
+                match = argsFull.match(rgxSHex);
                 if (mc(match)) {
-                    eHex = match[0].split(',');
-                    match = argsFull.match(rgxSpeed);
+                    sHex = match[0].split(',');
+                    match = argsFull.match(rgxEHex);
                     if (mc(match)) {
-                        speed = parseFloat(match[0]);
-                        match = argsFull.match(rgxTime);
-                        if (mc(match)) {
-                            time = match[0].split(':');
-                            match = argsFull.match(rgxETA);
-                            if (mc(match)) {
-                                eta = match[0].split(':');
-                            }
-                        }
+                        eHex = match[0].split(',');
                     }
+                }
+            }
+            //SPEED
+            match = argsFull.match(rgxSpeed);
+            if (mc(match)) {
+                speed = parseFloat(match[0]);
+            }
+            //STARGATE
+            match = argsFull.match(rgxStargate);
+            if (mc(match)) {
+                stargate = parseFloat(match[0]);
+            }
+            //TIMES
+            match = argsFull.match(rgxTime);
+            if (mc(match)) {
+                time = match[0].split(':');
+                match = argsFull.match(rgxETA);
+                if (mc(match)) {
+                    eta = match[0].split(':');
                 }
             }
 
@@ -697,11 +712,13 @@ Bot.on("message", (msg) => {
             if (speed === -1) return;
 
             //HEXES
-            if (sHex.length != 2 && eHex.length != 2) return;
-            sHex[0] = parseInt(sHex[0], 10);
-            sHex[1] = parseInt(sHex[1], 10);
-            eHex[0] = parseInt(eHex[0], 10);
-            eHex[1] = parseInt(eHex[1], 10);
+            if ((distance == -1 && sHex == -1 && eHex == -1) || (eHex.length == undefined && sHex.length == undefined && distance == -1) || (sHex.length != 2 && eHex.length != 2 && distance == -1)) return;
+            if (sHex.length === 2 && eHex.length === 2) {
+                sHex[0] = parseInt(sHex[0], 10);
+                sHex[1] = parseInt(sHex[1], 10);
+                eHex[0] = parseInt(eHex[0], 10);
+                eHex[1] = parseInt(eHex[1], 10);
+            }
 
             //CURRENT TIME
             var cTime = [0,0,0];
@@ -744,21 +761,54 @@ Bot.on("message", (msg) => {
             }
 
             //CALCULATIONS OF TRAVEL TIME
-            let distance = hexDistance(sHex[0],sHex[1],eHex[0],eHex[1]);
-            let hex_per_min = 60/(speed/3600);
-            var raw_time = ((distance*hex_per_min)/60).toFixed(3);
-            let days = ~~(raw_time/(24*3600));
-            raw_time %= 24*3600;
-            let hours = ~~(raw_time/3600);
-            raw_time %= 3600;
-            let minutes = ~~(raw_time/60);
-            raw_time %= 60;
-            let seconds = ~~raw_time;
-            raw_time %= 1;
-            let milseconds = Math.floor(raw_time*1000);
+            if (distance === -1) {
+                distance = hexDistance(sHex[0],sHex[1],eHex[0],eHex[1]);
+            }
+            var travel_once = [0,0,0,0,0];
+            var travel_once_stargate = [0,0,0,0,0];
+            var travel_twice = [0,0,0,0,0];
+            if (stargate === -1) {
+                let hex_per_min = 60/(speed/3600);
+                var raw_time = ((distance*hex_per_min)/60).toFixed(3);
+                let days = ~~(raw_time/(24*3600));
+                raw_time %= 24*3600;
+                let hours = ~~(raw_time/3600);
+                raw_time %= 3600;
+                let minutes = ~~(raw_time/60);
+                raw_time %= 60;
+                let seconds = ~~raw_time;
+                raw_time %= 1;
+                let milseconds = Math.floor(raw_time*1000);
+                travel_once = [days, hours, minutes, seconds, milseconds];
+                travel_twice = [days*2, hours*2, minutes*2, seconds*2, milseconds*2];
+            } else if (stargate > 0) {
+                var hex_per_min = 60/(speed/3600);
+                var raw_time = ((distance*hex_per_min)/60).toFixed(3);
+                var days = ~~(raw_time/(24*3600));
+                raw_time %= 24*3600;
+                var hours = ~~(raw_time/3600);
+                raw_time %= 3600;
+                var minutes = ~~(raw_time/60);
+                raw_time %= 60;
+                var seconds = ~~raw_time;
+                raw_time %= 1;
+                var milseconds = Math.floor(raw_time*1000);
+                travel_once = [days, hours, minutes, seconds, milseconds];
+                var hex_per_min = 60/((stargate+speed)/3600);
+                var raw_time = ((distance*hex_per_min)/60).toFixed(3);
+                var days = ~~(raw_time/(24*3600));
+                raw_time %= 24*3600;
+                var hours = ~~(raw_time/3600);
+                raw_time %= 3600;
+                var minutes = ~~(raw_time/60);
+                raw_time %= 60;
+                var seconds = ~~raw_time;
+                raw_time %= 1;
+                var milseconds = Math.floor(raw_time*1000);
+                travel_once_stargate = [days, hours, minutes, seconds, milseconds];
+                travel_twice = [days+travel_once[0], hours+travel_once[1], minutes+travel_once[2], seconds+travel_once[3], milseconds+travel_once[4]];
+            }
 
-            let travel_once = [days, hours, minutes, seconds, milseconds];
-            var travel_twice = [days*2, hours*2, minutes*2, seconds*2, milseconds*2];
             if (travel_twice[4] >= 1000) {
                 travel_twice[3] += 1;
                 travel_twice[4] -= 1000;
@@ -781,7 +831,11 @@ Bot.on("message", (msg) => {
             var pHome = [0,0,0,0,0];
             if (eta != -1 && time != -1) {
                 pTime = [cETA[0], cETA[1], cETA[2], cETA[3], 0];
-                pHome = [cETA[0]+travel_once[0], cETA[1]+travel_once[1], cETA[2]+travel_once[2], cETA[3]+travel_once[3], 0+travel_once[4]];
+                if (stargate === -1) {
+                    pHome = [cETA[0]+travel_once[0], cETA[1]+travel_once[1], cETA[2]+travel_once[2], cETA[3]+travel_once[3], 0+travel_once[4]];
+                } else {
+                    pHome = [cETA[0]+travel_once_stargate[0], cETA[1]+travel_once_stargate[1], cETA[2]+travel_once_stargate[2], cETA[3]+travel_once_stargate[3], 0+travel_once_stargate[4]];
+                }
 
                 do {
                     if (pHome[4] >= 1000) {
@@ -859,6 +913,58 @@ Bot.on("message", (msg) => {
                         pTime[1] -= 24;
                     }
                 } while (pTime >= 24);
+            } else {
+                pTime = [travel_once[0], travel_once[1], travel_once[2], travel_once[3], travel_once[4]];
+                pHome = [travel_twice[0], travel_twice[1], travel_twice[2], travel_twice[3], travel_twice[4]];
+                do {
+                    if (pHome[4] >= 1000) {
+                        pHome[3] += 1;
+                        pHome[4] -= 1000;
+                    }
+                } while (pHome[4] >= 1000);
+                do {
+                    if (pHome[3] >= 60) {
+                        pHome[2] += 1;
+                        pHome[3] -= 60;
+                    }
+                } while (pHome[3] >= 60);
+                do {
+                    if (pHome[2] >= 60) {
+                        pHome[1] += 1;
+                        pHome[2] -= 60;
+                    }
+                } while (pHome >= 60);
+                do {
+                    if (pHome[1] >= 24) {
+                        pHome[0] += 1;
+                        pHome[1] -= 24;
+                    }
+                } while (pHome >= 24);
+
+                do {
+                    if (pTime[4] >= 1000) {
+                        pTime[3] += 1;
+                        pTime[4] -= 1000;
+                    }
+                } while (pTime[4] >= 1000);
+                do {
+                    if (pTime[3] >= 60) {
+                        pTime[2] += 1;
+                        pTime[3] -= 60;
+                    }
+                } while (pTime[3] >= 60);
+                do {
+                    if (pTime[2] >= 60) {
+                        pTime[1] += 1;
+                        pTime[2] -= 60;
+                    }
+                } while (pTime >= 60);
+                do {
+                    if (pTime[1] >= 24) {
+                        pTime[0] += 1;
+                        pTime[1] -= 24;
+                    }
+                } while (pTime >= 24);
             }
 
             //EMBED CREATION
@@ -868,9 +974,14 @@ Bot.on("message", (msg) => {
             embed.setColor(0x5cc3ff);
             embed.setTitle('Travel Time Calculator');
             embed.setDescription(msg.content);
-            embed.addField(':regional_indicator_a: Starting Hex', `${sHex[0]} | ${sHex[1]}`, true);
-            embed.addField(':regional_indicator_b: Ending Hex', `${eHex[0]} | ${eHex[1]}`, true);
+            if (sHex != -1 && eHex != -1) {
+                embed.addField(':regional_indicator_a: Starting Hex', `${sHex[0]} | ${sHex[1]}`, true);
+                embed.addField(':regional_indicator_b: Ending Hex', `${eHex[0]} | ${eHex[1]}`, true);
+            }
             embed.addField(':stopwatch: Speed', `${speed}`, true);
+            if (stargate != -1) {
+                embed.addField(':stopwatch: Return Speed', `${speed+stargate}`, true);
+            }
             embed.addField(':left_right_arrow: Distance', `${distance}`, true);
             if (eta != -1 && time != -1) {
                 embed.addField(':clock10: Current Time', `${(cTime[0] <10)?'0'+cTime[0]:cTime[0]}:${(cTime[1] <10)?'0'+cTime[1]:cTime[1]}:${(cTime[2] <10)?'0'+cTime[2]:cTime[2]}`, false);
