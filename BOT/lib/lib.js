87e6b98917f8e121908aaf99172a167886ec16f8
MrMysterius <official.mrmysterius@gmail.com>
2020-06-05 19:37:18 +0200

    Fixed Refrences

--- BOT/lib/lib.js
+++ BOT/lib/lib.js
@@ -1,16 +1,6 @@
-const fs = require('fs');
-
-function e(n) {
-    return fs.existsSync(n);
-}
-
 let expt = {};
 
-if (e('./commands')) {
-    expt.cmd = require('./lib/commands/commands/commands.js');
-}
-if (e('./functions')) {
-    expt.fnc = require('./lib/functions/functions/functions.js');
-}
+expt.cmd = require('./commands/commands.js');
+expt.fnc = require('./functions/functions.js');
 
 module.exports = expt;
