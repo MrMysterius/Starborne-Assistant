d2bce1c71b0c706e4fd31d3e275f8a1fed3fe1da
MrMysterius <official.mrmysterius@gmail.com>
2020-06-06 23:11:41 +0200

    create-report-embed sourced out

--- BOT/index.js
+++ BOT/index.js
@@ -10,154 +10,6 @@ global.data = {
 };
 global.lib = require('./lib');
 
-/**
- * Makes an embed out of a Station Object
- * @param {Object} StationObject
- * @param {Object} DiscordMessageObject
- */
-function createEmbed(station, msg) {
-
-    try {
-        let metal = Bot.emojis.cache.find(emoji => emoji.name === 'sb_metal');
-        let gas = Bot.emojis.cache.find(emoji => emoji.name === 'sb_gas');
-        let crystal = Bot.emojis.cache.find(emoji => emoji.name === 'sb_crystal');
-
-        var embed = new Discord.MessageEmbed();
-        embed.setAuthor(msg.author.username, msg.author.displayAvatarURL());
-
-        embed.setTitle('Spy Report sent from '+msg.author.username);
-        embed.setDescription(station.description);
-
-        embed.setColor(0x4a6af7);
-
-        embed.setThumbnail(Bot.user.displayAvatarURL());
-
-        //FIELDS
-        //Hex
-        embed.addField('Hex', `${station.hex.x} | ${station.hex.y}`, true);
-
-        //Capture Defense
-        embed.addField('Capture Defense', `${station.capture_defense.has} / ${station.capture_defense.from}`, true);
-
-        //Resources
-        embed.addField('Station Resources', `${metal.toString()} ${station.resources.metal} ${gas.toString()} ${station.resources.gas} ${crystal.toString()} ${station.resources.crystal}`, false);
-        embed.addField('Station Hidden Resources', `${metal.toString()} ${station.resources_hidden.metal} ${gas.toString()} ${station.resources_hidden.gas} ${crystal.toString()} ${station.resources_hidden.crystal}`, true)
-
-        //Labor
-        embed.addField('Labor', `${station.fleet_labor} / ${station.labor}`, true);
-
-        //Station Cards
-        var cardsString = '';
-        if (station.cards.length === 0 || station.cards[0] === 'N/A') {
-            embed.addField('Station Cards', 'N/A', false);
-        } else {
-            for (var i=0;i<station.cards.length;i++) {
-                cardsString += `${station.cards[i].name}\n`;
-            }
-            embed.addField('Station Cards', `${cardsString}`, false);
-        }
-
-        //Buildings
-        var buildingString = '';
-        if (station.buildings.length === 0 || station.buildings[0] === 'N/A') {
-            embed.addField('Buildings', 'N/A', false);
-        } else {
-            for (var i=0;i<station.buildings.length;i++) {
-                buildingString += `${station.buildings[i].name} - Level ${station.buildings[i].level}\n`;
-            }
-            embed.addField('Buildings', `${buildingString}`, false);
-        }
-
-        //Outposts
-        var outpostString = '';
-        if (station.outposts.length === 0 || station.outposts[0] === 'N/A') {
-            embed.addField('Outposts', 'N/A', false);
-        } else {
-            for (var i=0;i<station.outposts.length;i++) {
-                outpostString += `${station.outposts[i].name} - Level ${station.outposts[i].level} - ${station.outposts[i].status}\n`;
-            }
-            embed.addField('Outposts', `${outpostString}`, false);
-        }
-
-        //Fleets
-        var fleetString = '';
-        if (station.fleets.length === 0 || station.fleets[0] === 'N/A') {
-            embed.addField('Fleets', 'N/A', false);
-        } else {
-            var counter = 1;
-            var tempString = '';
-            for (var i=0;i<station.fleets.length;i++) {
-                tempString = '';
-                if (station.fleets[i].cards != undefined) {
-                    tempString += `${station.fleets[i].count} ${station.fleets[i].type}\nCards: `;
-                    let length = station.fleets[i].cards.length;
-                    for (var l=0; l<length; l++) {
-                        if (l === length-1) {
-                            tempString += `${station.fleets[i].cards[l].name}\n`;
-                        } else {
-                            tempString += `${station.fleets[i].cards[l].name} | `;
-                        }
-                    }
-                } else {
-                    tempString += `${station.fleets[i].count} ${station.fleets[i].type}\n`;
-                }
-                if (fleetString.length + tempString.length >= 1020) {
-                    embed.addField(`Fleets ${counter}`, `${fleetString}`, false);
-                    counter++;
-                    fleetString = '';
-                }
-                fleetString += tempString;
-            }
-            if (counter === 1) {
-                embed.addField('Fleets', `${fleetString}`, false);
-            } else {
-                embed.addField(`Fleets ${counter}`, `${fleetString}`, false);
-                counter++;
-                fleetString = '';
-            }
-        }
-
-        //Construction Queues
-        var constructionString = '';
-        if (!(station.construction.buildprogress === 'N/A')) {
-            for (var i=0;i<station.construction.buildings.length;i++) {
-                constructionString += `${station.construction.buildings[i].name} - Level: ${station.construction.buildings[i].level}\n`;
-            }
-            embed.addField(`Building Queue ${station.construction.buildprogress}%`, constructionString, false);
-        }
-        constructionString = '';
-        if (!(station.construction.shipprogress === 'N/A')) {
-            for (var i=0;i<station.construction.ships.length;i++) {
-                constructionString += `${station.construction.ships[i].count} ${station.construction.ships[i].name}\n`;
-            }
-            embed.addField(`Ship Queue ${station.construction.shipprogress}%`, constructionString, false);
-        }
-
-        //Hangar
-        var hangarString = '';
-        if (station.hangar.length === 0 || station.hangar[0] === 'N/A') {
-            embed.addField('Hangar', 'N/A', false);
-        } else {
-            for (var i=0;i<station.hangar.length;i++) {
-                hangarString += `${station.hangar[i].name} ${station.hangar[i].count}\n`;
-            }
-            embed.addField('Hangar', `${hangarString}`, false);
-        }
-
-        //Fleet Stats
-        embed.addField(`Min Fleet Stats`, `FP: ${parseInt(station.fleet_fp,10)}\nHP: ${parseInt(station.fleet_hp,10)}\nCargo: ${parseInt(station.fleet_cargo,10)}\nBombing: ${parseInt(station.fleet_bombing,10)}`);
-
-        console.log(`# ${msg.guild.name}#${msg.channel.name}|${msg.channel.id} - ${msg.author.username}#${msg.author.discriminator}`);
-    }
-    catch (err) {
-        var embed = 'ERROR';
-        console.log('ERROR', station);
-        console.log(err);
-    }
-    return embed;
-}
-
-
 
 //WEBSOCKETSERVER
 var reports = [];
@@ -228,7 +80,7 @@ Bot.on("message", (msg) => {
 
                 if (station != 'error') {
                     msg.delete();
-                    let embed = createEmbed(station, msg);
+                    let embed = lib.fnc.createReportEmbed(station, msg);
                     try {
                         msg.channel.send(embed);
                     }
@@ -251,7 +103,7 @@ Bot.on("message", (msg) => {
             if (msg.content.startsWith(reports[i].code)) {
                 if (msg.content.includes('DEBUG')) console.log(reports[i].data);
                 try {
-                    let embed = createEmbed(reports[i].data, msg);
+                    let embed = lib.fnc.createReportEmbed(reports[i].data, msg);
                     msg.channel.send(embed)
                 }
                 catch (err) {
--- BOT/lib/functions/functions.js
+++ BOT/lib/functions/functions.js
@@ -5,5 +5,6 @@ expt.calcShipInfo = require('./calc-ships-raw.js');
 expt.getStationInformation = require('./create-station.js');
 expt.hexDistance = require('./hex-distance.js');
 expt.getCardStats = require('./get-card-stats.js');
+expt.createReportEmbed = require('./create-report-embed.js');
 
 module.exports = expt;
