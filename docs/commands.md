40a1b18b397735dfd153bcae8ca8987d51e0ba6a
Mr Mysterius <official.mrmysterius@gmail.com>
2020-07-15 14:45:32 +0200

    Rewrite Basicly Done

--- docs/commands.md
+++ docs/commands.md
@@ -3,12 +3,14 @@
 ## Command Directorys Config
 
 DirObject
+
 - type: absolute | relative
 - path: dir-path
 
 abolute means a path that is abosulte and not relative, like the relative type, to the current working directory of the process.
 
 This is the standard path
+
 ```json
     {
         "type": "relative",
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//SETUP FOLDERS IF THEY DON'T EXIST YET
+const fs = require('fs');
+const path = require('path');
+if (!fs.existsSync(path.join(process.cwd(), 'temp'))) fs.mkdirSync(path.join(process.cwd(), 'temp'));
+if (!fs.existsSync(path.join(process.cwd(), 'database'))) fs.mkdirSync(path.join(process.cwd(), 'database'));
+if (!fs.existsSync(path.join(process.cwd(), 'logs'))) fs.mkdirSync(path.join(process.cwd(), 'logs'));
+
 const CommandsHandler = require('./lib/classes/CommandHandler.js');
 global.commands = new CommandsHandler(require('./config/commands-directorys.json'));
 
